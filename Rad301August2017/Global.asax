8de9f8ffab615610f2fdcd09fb75206c3d79ef61
ItsPaul <powell.paul@itsligo.ie>
2017-08-27 21:40:03 +0100

    Add project files.

---
+++ Rad301August2017/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Rad301August2017.MvcApplication" Language="C#" %>
---
+++ Rad301August2017/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Rad301August2017.Startup))]
+namespace Rad301August2017
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
